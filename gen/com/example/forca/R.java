1336113295d2ef52361aab87d0743d7b5143a6ba
Kunioshi <filipekunioshi@hotmail.com>
2013-09-14 20:15:15 -0300

    Adicionando as imagens da Forca/Enforcado. Adicionando XML "activity_forca" com Linear Layout Horizontal com 2 Linear Layout Verticais(uma para as imagens da forca e a outra para as letras e entrada do usuário)
    PS: O "activity_forca.xml" ainda não está substituindo o
    "activity_main.xml"

--- gen/com/example/forca/R.java
+++ gen/com/example/forca/R.java
@@ -21,13 +21,24 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int forca=0x7f020000;
+        public static final int forca0=0x7f020001;
+        public static final int forca1=0x7f020002;
+        public static final int forca2=0x7f020003;
+        public static final int forca3=0x7f020004;
+        public static final int forca4=0x7f020005;
+        public static final int forca5=0x7f020006;
+        public static final int forca6=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int viewImgForca=0x7f080000;
+        public static final int viewTxtForca=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_forca=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
