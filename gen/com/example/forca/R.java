3a0dfc2329aee95a26e93c49c388d8b22b89c2a8
Kunioshi <filipekunioshi@hotmail.com>
2013-09-14 23:58:19 -0300

    Adicionando botão de Submit e TextView para mostrar as letras falhadas

--- gen/com/example/forca/R.java
+++ gen/com/example/forca/R.java
@@ -32,9 +32,11 @@ public final class R {
         public static final int ic_launcher=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080006;
+        public static final int button1=0x7f080005;
         public static final int imgForca=0x7f080001;
         public static final int txtResposta=0x7f080003;
+        public static final int txtTentativas=0x7f080004;
         public static final int viewImgForca=0x7f080000;
         public static final int viewTxtForca=0x7f080002;
     }
