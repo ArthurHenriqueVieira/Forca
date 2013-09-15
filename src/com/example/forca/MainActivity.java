a5ce903cf6f754893e617a1b8f32a3d2293cf7f6
Tutz <ttutty.oliveira@gmail.com>
2013-09-14 21:18:46 -0300

    Mudanças na Main; Criada classe que recebe XML "activity_forca"

--- src/com/example/forca/MainActivity.java
+++ src/com/example/forca/MainActivity.java
@@ -2,14 +2,37 @@ package com.example.forca;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	
+	//Cria os botões
+	Button btnJogar;
+	Button btnCreditos;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		btnJogar = (Button)findViewById(R.id.btnJogar);
+        
+        if(btnJogar !=  null) {
+        	
+        	btnJogar.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View arg0) {
+					Intent i = new Intent();
+        			i.setClass(getApplicationContext(), Forca.class);
+        			startActivity(i);
+					
+				}
+        	});
+        }
 	}
 
 	@Override
